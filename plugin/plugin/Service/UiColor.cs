3df742c8a94fb894348236e2ed4feafc973bc7a5
Sevii <sevii@sevii.dev>
2026-05-16 01:06:02 +0200

    improve uicolor

--- plugin/plugin/Service/UiColor.cs
+++ plugin/plugin/Service/UiColor.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using Dalamud.Hooking;
+using FFXIVClientStructs.FFXIV.Component.GUI;
 
 namespace Aetherment;
 
@@ -13,40 +14,76 @@ public class UiColor: IDisposable {
 		public uint clr;
 	}
 	
-	private Dictionary<(bool, uint), uint> colors;
+	private List<AtkUIColorHolder.UIColor> originalColors;
+	// private Dictionary<(bool, uint), uint> colors;
 	
 	public unsafe UiColor() {
-		colors = new();
+		originalColors = new();
+		var c = AtkStage.Instance()->AtkUIColorHolder;
+		for(var i = 0; i < c->UIColors.Count; i++) {
+			originalColors.Add(c->UIColors[i]);
+		}
 		
-		UiColorHandlerHook = Aetherment.HookProv.HookFromAddress<UiColorHandlerDelegate>(Aetherment.SigScanner.ScanText("4C 8B 91 ?? ?? ?? ?? 4C 8B D9 49 8B 02"), UiColorHandler);
-		UiColorHandlerHook.Enable();
+		// dont use a delegate since a lot of elements dont actually use it???
+		// colors = new();
+		
+		// UiColorHandlerHook = Aetherment.HookProv.HookFromAddress<UiColorHandlerDelegate>(Aetherment.SigScanner.ScanText("4C 8B 91 ?? ?? ?? ?? 4C 8B D9 49 8B 02"), UiColorHandler);
+		// UiColorHandlerHook.Enable();
 		
 		setUiColors = SetUiColors;
 	}
 	
-	public void Dispose() {
-		UiColorHandlerHook.Dispose();
+	public unsafe void Dispose() {
+		ResetColors();
+		
+		// UiColorHandlerHook.Dispose();
 	}
 	
-	private unsafe delegate uint UiColorHandlerDelegate(nint self, byte use_theme, uint index);
-	private Hook<UiColorHandlerDelegate> UiColorHandlerHook;
-	private uint UiColorHandler(nint self, byte use_theme, uint index) {
-		lock(colors) {
-			if(colors.TryGetValue((use_theme != 0, index), out var color))
-				return color;
+	// private unsafe delegate uint UiColorHandlerDelegate(nint self, byte use_theme, uint index);
+	// private Hook<UiColorHandlerDelegate> UiColorHandlerHook;
+	// private uint UiColorHandler(nint self, byte use_theme, uint index) {
+	// 	lock(colors) {
+	// 		if(colors.TryGetValue((use_theme != 0, index), out var color)) {
+	// 			return color;
+	// 		}
+	// 	}
+	// 	
+	// 	return UiColorHandlerHook.Original(self, use_theme, index);
+	// }
+	
+	private unsafe void ResetColors() {
+		var c = AtkStage.Instance()->AtkUIColorHolder;
+		c->UIColors.Clear();
+		for(var i = 0; i < originalColors.Count; i++) {
+			c->UIColors.AddCopy(originalColors[i]);
 		}
-		
-		return UiColorHandlerHook.Original(self, use_theme, index);
 	}
 	
 	public SetUiColorsDelegate setUiColors;
 	public unsafe delegate void SetUiColorsDelegate(Color* array, nint length);
 	public unsafe void SetUiColors(Color* array, nint length) {
-		lock(colors) {
-			colors.Clear();
-			for(int i = 0; i < length; i++) {
-				var color = array[i];
-				colors.Add((color.useTheme != 0, color.index), color.clr);
+		// lock(colors) {
+		// 	colors.Clear();
+		// 	for(int i = 0; i < length; i++) {
+		// 		var color = array[i];
+		// 		colors.Add((color.useTheme != 0, color.index), color.clr);
+		// 	}
+		// }
+		
+		ResetColors();
+		var c = AtkStage.Instance()->AtkUIColorHolder;
+		for(int i = 0; i < length; i++) {
+			var color = array[i];
+			for(var j = 0; j < c->UIColors.Count; j++) {
+				if(c->UIColors[j].RowId == color.index) {
+					c->UIColors[j].Color = color.clr;
+					
+					// if(color.useTheme != 0) {
+					// 	c->UIColors[j].ThemedColor = color.clr;
+					// } else {
+					// 	c->UIColors[j].Color = color.clr;
+					// }
+				}
 			}
 		}
 	}
